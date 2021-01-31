a83faddb9806dabf631f0866ce9127b8d0e60ff9
Shamnas <shamnastv@gmail.com>
2021-01-31 11:05:35 +0530

    update

---
+++ 1.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#SBATCH --job-name=GNN # Job name
+#SBATCH --ntasks=4 # Run on a single CPU
+#SBATCH --time=23:50:00 # Time limit hrs:min:sec
+#SBATCH --output=test_job%j.out # Standard output and error log
+#SBATCH --gres=gpu:1
+#SBATCH --partition=cl1_48h-1G
+
+python3 main.py
+
