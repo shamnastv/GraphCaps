2f4453eaa494aca17d2d24e6a7ffecf78e1021ca
Shamnas <shamnastv@gmail.com>
2021-01-31 11:12:28 +0530

    update

--- 1.sh
+++ 1.sh
@@ -4,7 +4,7 @@
 #SBATCH --time=23:50:00 # Time limit hrs:min:sec
 #SBATCH --output=test_job%j.out # Standard output and error log
 #SBATCH --gres=gpu:1
-#SBATCH --partition=cl1_48h-1G
+#SBATCH --partition=cl2_48h-1G
 
 python3 main.py
 
