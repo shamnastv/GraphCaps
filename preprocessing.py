c1da962bcfed9cdfbea9222fa6f12af0f124fcf1
Shamnas <shamnastv@gmail.com>
2021-01-31 10:34:56 +0530

    update

--- preprocessing.py
+++ preprocessing.py
@@ -110,6 +110,7 @@ class GraphDataset():
                         if len(id_to_attri_maps) <= attri_idx_all:
                             id_to_attri_maps.append([])
                             attri_to_id_maps.append(dict())
+                        print(id_to_attri_maps[attri_idx_all])
                         if attri in set(id_to_attri_maps[attri_idx_all]):
                             continue
                         else:
