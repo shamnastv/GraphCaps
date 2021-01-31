8421750c808920a2bc3ab642d16b5f652c8b85a0
Shamnas <shamnastv@gmail.com>
2021-01-31 21:50:07 +0530

    update

--- model.py
+++ model.py
@@ -100,7 +100,7 @@ class Model(nn.Module):
         for layer in self.gcn_layers:
             features = layer(adj_norm, features)
             features = torch.tanh(features)
-            features = self.dropout(features)
+            # features = self.dropout(features)
             hidden_representations.append(features.reshape(b, n, c, -1))
 
         hidden_representations = torch.cat(hidden_representations, dim=2)  # b x n x c x d
