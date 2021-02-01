c3c654b23ac23b50a71c783495ec8834bcfe1bdc
Shamnas <shamnastv@gmail.com>
2021-02-01 17:28:14 +0530

    update

--- 2.sh
+++ 2.sh
@@ -4,7 +4,7 @@
 #SBATCH --time=23:50:00 # Time limit hrs:min:sec
 #SBATCH --output=test_job%j.out # Standard output and error log
 #SBATCH --gres=gpu:1
-#SBATCH --partition=cl2_48h-1G
+#SBATCH --partition=cl1_48h-1G
 
 python3 preprocessing.py --dataset_input_dir graph_gexf/REDDIT-MULTI-5K
 python3 main.py --dataset_dir data_plk/REDDIT-MULTI-5K
